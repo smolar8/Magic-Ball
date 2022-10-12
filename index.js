06284ca5fdf690ce9dab026a4ce07cb7f1a097c0
unknown <smolar2@interia.pl>
2022-10-12 10:56:07 +0200

    file css

---
+++ index.js
@@ -0,0 +1,4 @@
+const ball = document.querySelector(".ball-img");
+const input = document.querySelector("input");
+const answer = document.querySelector(".answer");
+const error = document.querySelector(".error");
