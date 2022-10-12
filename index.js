1e61d82ec30c0a86a396e3412733717ca682bf05
unknown <smolar2@interia.pl>
2022-10-12 14:33:05 +0200

    create js

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ const ball = document.querySelector(".ball-img");
 const input = document.querySelector("input");
 const answer = document.querySelector(".answer");
 const error = document.querySelector(".error");
+
+const answersArr = [
+  "Tak!",
+  "Nie.",
+  "Może.",
+  "Ciężko powiedzieć ...",
+  "Nie chcesz znać odpowiedzi na to pytanie...:/",
+];
+
+const shakeBall = () => {
+  ball.classList.add("shake-animaton");
+  setTimeout(checkInput, 1000);
+};
+
+const checkInput = () => {
+  if (input.value !== "" && input.value.slice(-1) === "?") {
+    generateAnswer();
+    error.textContent = "";
+  } else if (input.value !== "" && input.value.slice(-1) !== "?") {
+    error.textContent = 'Pytanie musi być zakończone znakiem "?".';
+    answer.textContent = "";
+  } else {
+    error.textContent = "Musisz zadać jakieś pytanie!";
+    answer.textContent = "";
+  }
+  ball.classList.remove("shake-animaton");
+};
+
+const generateAnswer = () => {
+  const number = Math.floor(Math.random() * 5);
+  answer.innerHTML = `<span>Odpowiedź: </span>${answersArr[number]}`;
+};
+
+ball.addEventListener("click", shakeBall);
